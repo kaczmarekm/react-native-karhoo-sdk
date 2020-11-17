ab88700f8918d15318cc2a046be8826aa6fe112e
mkaczmarek <mkaczmarek@iterato.rs>
2020-11-17 10:54:41 +0100

    ios booking working

--- ios/KarhooSdk.m
+++ ios/KarhooSdk.m
@@ -1,8 +1,8 @@
-#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
 
 @interface RCT_EXTERN_MODULE(KarhooSdk, NSObject)
     RCT_EXTERN_METHOD(initialize:(NSString *)identifier referer:(NSString *)referer organisationId:(NSString *)organisationId)
-    RCT_EXTERN_METHOD(getPaymentNonce:(NSString *)organisationId currency:(NSString *)currency)
-    RCT_EXTERN_METHOD(passenger:(NSDictionary *)userInfo quoteId:(NSString *)quoteId paymentNonce:(NSString *)paymentNonce)
+    RCT_EXTERN_METHOD(getPaymentNonce:(NSString *)organisationId paymentData:(NSDictionary *)paymentData)
+    RCT_EXTERN_METHOD(bookTrip:(NSDictionary *)passenger quoteId:(NSString *)quoteId paymentNonce:(NSString *)paymentNonce)
 @end
 
--- ios/KarhooSdk.swift
+++ ios/KarhooSdk.swift
@@ -4,58 +4,86 @@ import Braintree
 
 @objc(KarhooSdk)
 class KarhooSdk: NSObject {
-    @objc(initialize:referer:organisationId:)
-    func initialize(identifier: String, referer: String, organisationId: String) -> Void {
+    @objc func initialize(_ identifier: String, referer: String, organisationId: String) -> Void {
         Karhoo.set(configuration: KarhooConfiguration(identifier: identifier, referer: referer, organisationId: organisationId))
     }
 
-    @objc(getPaymentNonce:currency:resolver:rejecter:)
-    func getPaymentNonce(organisationId: String, currency: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
-        Karhoo.getPaymentService().initialisePaymentSDK(paymentSDKTokenPayload: PaymentSDKTokenPayload(organisationId: organisationId, currency: currency))
+    @objc func getPaymentNonce(_ organisationId: String, paymentData: NSDictionary, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+        Karhoo
+            .getPaymentService()
+            .initialisePaymentSDK(paymentSDKTokenPayload: PaymentSDKTokenPayload(organisationId: organisationId, currency: paymentData["currency"] as! String))
             .execute { result in
                 switch result {
                     case .success(let sdk):
-                        let request =  BTDropInRequest()
-                        let dropIn = BTDropInController(authorization: sdk.token, request: request) {
+                        let amountString = paymentData["amount"] as! String
+                        let amountDecimal = Decimal(string: amountString)
+                        
+                        if (amountDecimal == nil) {
+                            print("KarhooSdk ERROR: amount not formatted correctly")
+                            reject("KarhooSdk ERROR", "amount not formatted correctly", nil)
+                            return;
+                        }
+                        
+                        let amountNSDecimalNumber = NSDecimalNumber(decimal: amountDecimal!)
+                        
+                        let threeDSecureRequest = BTThreeDSecureRequest()
+                        threeDSecureRequest.amount = amountNSDecimalNumber
+                        threeDSecureRequest.versionRequested = .version2
+                        
+                        let dropInRequest =  BTDropInRequest()
+                        dropInRequest.threeDSecureVerification = true
+                        dropInRequest.threeDSecureRequest = threeDSecureRequest
+                        
+                        let dropIn = BTDropInController(authorization: sdk.token, request: dropInRequest) {
                             (controller, result, error) in
                                 if (error != nil) {
-                                    print("KarhooSdk ERROR \(error.code) \(error.message)")
-                                    controller.dismiss(animated: true, completion: nil)                            
-                                    reject(nil, nil, nil)
+                                    if let unwrappedError = error {
+                                        print("KarhooSdk ERROR: \(unwrappedError)")
+                                    } else {
+                                        print("KarhooSdk ERROR: BTDropInController error")
+                                    }
+                                    controller.dismiss(animated: true, completion: nil)
+                                    reject("KarhooSdk ERROR", nil, error)
                                 } else if (result?.isCancelled == true) {
                                     print("KarhooSdk CANCELLED")
-                                    controller.dismiss(animated: true, completion: nil)                            
-                                    reject(nil, nil, nil)
+                                    controller.dismiss(animated: true, completion: nil)
+                                    reject("KarhooSdk ERROR", "CANCELLED", nil)
                                 } else if let result = result {
                                     print("KarhooSdk SUCCESS")
-                                    var nonce = result.paymentMethod?.nonce
-                                    var resultDict: NSDictionary = [   
-                                        "nonce": nonce!,                                                                     
+                                    let nonce = result.paymentMethod?.nonce
+                                    let resultDict: NSDictionary = [
+                                        "nonce": nonce!,
                                     ]
-                                    controller.dismiss(animated: true, completion: nil)     
-                                    resolve(resultDict)                       
+                                    controller.dismiss(animated: true, completion: nil)
+                                    resolve(resultDict)
                                 }
                         }
                         let rootViewController = UIApplication.shared.delegate?.window??.rootViewController
                         rootViewController?.present(dropIn!, animated: true, completion: nil)
                     case .failure(let error):
-                        print("KarhooSdk ERROR: \(error?.code) \(error?.message)")
-                        reject(nil, nil, nil)
+                        if let unwrappedError = error {
+                            print("KarhooSdk ERROR: \(unwrappedError.code) \(unwrappedError.message)")
+                        } else {
+                            print("KarhooSdk ERROR: Karhoo.getPaymentService().initialisePaymentSDK() error")
+                        }
+                        reject("KarhooSdk ERROR", nil, error)
                 }
             }
     }
 
-    @objc(bookTrip:quoteId:paymentNonce:)
-    func bookTrip(passenger: NSDictionary, quoteId: String, paymentNonce: String, resolver resolve: @escaping RCTPromiseRejectBlock, rejecter reject: @escaping RCTPromiseResolveBlock) -> Void {
-        let tripService = Karhoo.getTripService()
-        let passengerDetails = PassengerDetails(
-            firstName: passenger["firstName"],
-            lastName: passenger["lastName"],
-            email: passenger["email"],
-            mobileNumber: passenger["mobileNumber"],
-            locale: passenger["locale"]
+    @objc func bookTrip(_ passenger: NSDictionary, quoteId: String, paymentNonce: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+        let passengers = Passengers(
+            additionalPassengers: 0,
+            passengerDetails: [
+                PassengerDetails(
+                    firstName: passenger["firstName"] as! String,
+                    lastName: passenger["lastName"] as! String,
+                    email: passenger["email"] as! String,
+                    phoneNumber: passenger["mobileNumber"] as! String,
+                    locale: passenger["locale"] as! String
+                )
+            ]
         )
-        let passengers = Passengers(additionalPassengers: 0, passengerDetails: [passengerDetails])
         let tripBooking = TripBooking(
             quoteId: quoteId,
             passengers: passengers,
@@ -63,19 +91,26 @@ class KarhooSdk: NSObject {
             paymentNonce: paymentNonce,
             comments: nil
         )
-        tripService.book(tripBooking: tripBooking).execute { result in
-            switch result {
-                case .success(let trip):
-                    print("KarhooSdk SUCCESS: \(trip)")
-                    var resultDict: NSDictionary = [   
-                        "tripId": trip,                                                                     
-                    ]
-                    resolve(resultDict)
-                case .failure(let error):
-                    print("KarhooSdk ERROR: \(error.code) \(error.message)")
-                    reject(nil, nil, nil)
+        Karhoo
+            .getTripService()
+            .book(tripBooking: tripBooking)
+            .execute { result in
+                switch result {
+                    case .success(let trip):
+                        print("KarhooSdk SUCCESS: \(trip)")
+                        let resultDict: NSDictionary = [
+                            "tripId": trip,
+                        ]
+                        resolve(resultDict)
+                    case .failure(let error):
+                        if let unwrappedError = error {
+                            print("KarhooSdk ERROR: \(unwrappedError.code) \(unwrappedError.message)")
+                        } else {
+                            print("KarhooSdk ERROR: Karhoo.getTripService().book() error")
+                        }
+                        reject("KarhooSdk ERROR", nil, error)
+                }
             }
-        }
     }
 }
 
