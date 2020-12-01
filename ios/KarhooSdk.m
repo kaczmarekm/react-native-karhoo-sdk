3f8dbdd5894b7a1bac9abb1b90544a5a7e564478
mkaczmarek <mkaczmarek@iterato.rs>
2020-12-01 16:14:56 +0100

    fix ios methods

--- ios/KarhooSdk.m
+++ ios/KarhooSdk.m
@@ -2,7 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(KarhooSdk, NSObject)
     RCT_EXTERN_METHOD(initialize:(NSString *)identifier referer:(NSString *)referer organisationId:(NSString *)organisationId)
-    RCT_EXTERN_METHOD(getPaymentNonce:(NSString *)organisationId paymentData:(NSDictionary *)paymentData)
-    RCT_EXTERN_METHOD(bookTrip:(NSDictionary *)passenger quoteId:(NSString *)quoteId paymentNonce:(NSString *)paymentNonce)
+    RCT_EXTERN_METHOD(getPaymentNonce:(NSString *)organisationId paymentData:(NSDictionary *)paymentData resolve:(RCTPromiseResolveBlock *)resolve reject:(RCTPromiseRejectBlock *)reject)
+    RCT_EXTERN_METHOD(bookTrip:(NSDictionary *)passenger quoteId:(NSString *)quoteId paymentNonce:(NSString *)paymentNonce resolve:(RCTPromiseResolveBlock *)resolve reject:(RCTPromiseRejectBlock *)reject)
 @end
 
